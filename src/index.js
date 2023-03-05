4785b286bc1071df9e66c013c85a3acf8daf7712
Leonardo <34871458+LeozinH1@users.noreply.github.com>
2023-03-05 00:28:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    var address = socket.handshake.address;
+    var address = socket.handshake.address.address;
     io.emit("chat message", msg, address);
   });
 });
