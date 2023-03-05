9431d2e52b30bf633550d40430964956cd7855fa
Leonardo <34871458+LeozinH1@users.noreply.github.com>
2023-03-05 00:53:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,10 @@ app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
   var sHeaders = socket.handshake.headers;
   console.info("[%s] connected!", sHeaders["x-forwarded-for"]);
-});
 
-io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
